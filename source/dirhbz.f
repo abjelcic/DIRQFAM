9081e6c8ed87f48d0298df7c105a0043e3a72969
Antonio <abjelcic@phy.hr>
2019-12-30 00:45:17 +0100

    Out of bounds bug fixed

--- source/dirhbz.f
+++ source/dirhbz.f
@@ -12092,8 +12092,14 @@ c           s1 = s2
                   if (nz1.eq.nz2)   szz = (nz1+0.5d0)
                endif
                if (nz1.eq.nz2) then
-                  if (nr1.eq.nr2+1) srp = -sq(nr1*(nr1+ml1))
-                  if (nr1.eq.nr2-1) srp = -sq((nr1+1)*(nr1+ml1+1))
+c=======================================================================c-----!abjelcic
+                  if (nr1.eq.nr2+1) then                                c
+                      srp = -DSQRT(DBLE(nr1*(nr1+ml1)));                c
+                  endif                                                 c
+                  if (nr1.eq.nr2-1) then                                c
+                      srp = -DSQRT(DBLE((nr1+1)*(nr1+ml1+1)));          c
+                  endif                                                 c
+c=======================================================================c-----!abjelcic
                   if (nr1.eq.nr2)   srp = (nr1+1)
                endif
             endif
